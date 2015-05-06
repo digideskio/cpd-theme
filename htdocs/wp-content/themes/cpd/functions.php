a83211cceba9c9e134ab27f9423b85c14602ac68
David Green <davetgreen@GALLIFREY.local>
2015-05-06 14:28:54 +0100

    Renamed theme.

---
+++ htdocs/wp-content/themes/cpd/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Enqueue scripts & styles
+ */
+function cpd_enqueue_assets()
+{
+    // Parent theme CSS
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    // Child header JS
+    wp_enqueue_script( 'header', get_stylesheet_directory_uri() . '/js/header.min.js', array(), '0.1', false );
+
+    // Child footer JS
+    wp_enqueue_script( 'footer', get_stylesheet_directory_uri() . '/js/footer.min.js', array('jquery', 'jquery-ui'), '0.1', true );
+}
+add_action( 'wp_enqueue_scripts', 'cpd_enqueue_assets' );
+
+/**
+ * Remove from Twenty Fifteen
+ */
+
+function cpd_cleanup_twentyfifteen()
+{
+    remove_theme_support('custom-header');
+    remove_theme_support('custom-background');
+}
+add_action( 'init', 'cpd_cleanup_twentyfifteen' );
