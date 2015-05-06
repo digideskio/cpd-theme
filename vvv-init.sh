f356388246809a0158423d28f4d55b2500feaa8b
David Green <davetgreen@GALLIFREY.local>
2015-05-06 13:42:46 +0100

    Tweaked init accordingly.

--- vvv-init.sh
+++ vvv-init.sh
@@ -5,7 +5,7 @@ echo "Commencing cpd-theme Setup"
 # Make a database, if we don't already have one
 echo "Creating database (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS cpd-theme"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON cpd-theme.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON cpd_theme.* TO wp@localhost IDENTIFIED BY 'wp';"
 
 # Download WordPress
 if [ ! -d htdocs/wp-admin ]
@@ -13,7 +13,7 @@ then
 	echo "Installing WordPress using WP CLI"
 	cd htdocs
 	wp core download --allow-root
-	wp core config --dbname="cpd-theme" --dbuser=wp --dbpass=wp --dbhost="localhost" --allow-root
+	wp core config --dbname="cpd_theme" --dbuser=wp --dbpass=wp --dbhost="localhost" --allow-root
 	wp core install --url=cpd-theme.dev --title="CPD Theme" --admin_user=admin --admin_password=password --admin_email=hello@makedo.in --allow-root
 	cd ..
 fi
