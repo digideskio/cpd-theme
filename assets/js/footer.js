a8b61730530d5afbfb28ca16919afbb9d3bcbc11
Dave Green <hello@davetgreen.me>
2015-05-31 19:37:49 +0100

    Table and print styling complete.

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,1 +1,21 @@
 // Footer Scripts
+
+$ = jQuery;
+
+var thValues = [];
+
+// Get the table heading values
+$('#main table th').each(function(e) {
+  thValues.push($(this).text());
+});
+
+// Loop through each row and add the data
+// attribute to each cell
+$('#main .odd, #main .even').each(function(e) {
+
+  var cells = $(this).children("td");
+
+  for (var i = cells.length - 1; i >= 0; i--) {
+    $(cells[i]).attr("data-th-value", thValues[i]);
+  };
+});
