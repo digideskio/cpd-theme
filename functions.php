3868c6e3d7ab92c1575653d88e6595240bafcac0
David Green <davetgreen@GALLIFREY.local>
2015-05-31 15:11:18 +0100

    Changed footer script enqueueing - it now enqueues as it should.

--- functions.php
+++ functions.php
@@ -53,6 +53,6 @@ function cpd_enqueue_assets()
     wp_enqueue_script('header', get_stylesheet_directory_uri() . '/js/header.min.js', array(), '0.1', false);
 
     // Child footer JS
-    wp_enqueue_script('footer', get_stylesheet_directory_uri() . '/js/footer.min.js', array('jquery', 'jquery-ui'), '0.1', true);
+    wp_enqueue_script('footer', get_stylesheet_directory_uri() . '/js/footer.min.js', array(), '0.1', true);
 }
 add_action('wp_enqueue_scripts', 'cpd_enqueue_assets');
